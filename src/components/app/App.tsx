51cf204678463528f0111f2cdd3fd0b17ed3bb1e
Seyhahaa <jbseyha@gmail.com>
2024-01-11 11:04:15 +0700

    Finish Hero

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import "./style.css";
 import DefaultLayout from "../layout/defaultLayout";
 import Nav from "../nav";
+import Hero from "../hero";
 
 const App: React.FC = ()=>{
   return (
     <>
       <DefaultLayout>
           <Nav/>
+          <div className="content">
+            <Hero />
+
+          </div>
+          
       </DefaultLayout>
     </>
   )
---
+++ src/components/hero/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+interface HeroProps {}
+const Hero: React.FC<HeroProps> = () =>{
+    return (
+        <>
+            <div className="hero">
+
+            </div>
+        </>
+    )
+}
+export default Hero;
