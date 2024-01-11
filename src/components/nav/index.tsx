7dc04d9f9ebce483d749d3fd3e47a7ce72656587
Seyhahaa <jbseyha@gmail.com>
2024-01-11 10:01:02 +0700

    Update menu List

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -2,15 +2,31 @@ import React from "react";
 import "./style.css";
 
 interface NavProps {}
+const menu = [
+    {
+        link: "#about",
+        label: "About"
+    },
+    {
+        link: "#menu",
+        label: "Menu"
+    },
+    {
+        link: "#contact",
+        label: "Contact"
+    },
+];
 const Nav: React.FC<NavProps> = () => {
     // const {} = props;
     return (
         <nav>
             <a href="#" className="logo" >food website</a>
             <ul>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Menu</a></li>
-                <li><a href="#">Contact</a></li>
+                {
+                    menu.map((item,index) => (
+                        <li key={index}><a href={item.link}>{item.label}</a></li>
+                    ))
+                }
             </ul>
         </nav>
     )
